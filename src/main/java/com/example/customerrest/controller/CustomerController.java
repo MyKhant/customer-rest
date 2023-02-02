1e3f95126a0f158033298cebeab2b9a8ec1f8031
MyKhant <myokhantkyaw148@gmail.com>
2023-02-02 11:40:56 +0630

    Second Upload Rest api

--- src/main/java/com/example/customerrest/controller/CustomerController.java
+++ src/main/java/com/example/customerrest/controller/CustomerController.java
@@ -1,11 +1,15 @@
 package com.example.customerrest.controller;
 
 import com.example.customerrest.ds.Customer;
+import com.example.customerrest.ds.CustomerDto;
 import com.example.customerrest.service.CustomerService;
+import org.apache.catalina.loader.ResourceEntry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,23 +26,32 @@ public class CustomerController {
     public String init(){
 
         List.of(
-                new Customer("TH","Thomas","Hardy","thomas@gmail.com"),
-                new Customer("JW","John","William","william@gmail.com"),
-                new Customer("JD","John","Doe","john@gmail.com")
+                new CustomerDto(null,"TH","Thomas","Hardy","thomas@gmail.com"),
+                new CustomerDto(null,"JW","John","William","william@gmail.com"),
+                new CustomerDto(null,"JD","John","Doe","john@gmail.com")
         )
                 .forEach(customerService::saveCustomer);
         return "success";
     }
 
-    @GetMapping("/customers/all")
+    @GetMapping(value = "/customers/all", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public Iterable<Customer> listAllCustomers(){
+    public List<CustomerDto> listAllCustomers(){
         return customerService.listCustomers();
     }
 
     @GetMapping("/customers")
-    public ResponseEntity<Iterable<Customer>> listCustomers(){
-        return ResponseEntity.ok()
+    public ResponseEntity<List<CustomerDto>> listCustomers(){
+        return ResponseEntity
+                .accepted()
+                .contentType(MediaType.valueOf(MediaType.APPLICATION_JSON_VALUE))
                 .body(customerService.listCustomers());
+    }
+
+    @GetMapping("/customers/customer")
+    public ResponseEntity<CustomerDto> customerById(@RequestParam("id")int id){
+        return ResponseEntity
+                .ok()
+                .body(customerService.findCustomerById(id));
     }
 }
--- src/main/java/com/example/customerrest/service/CustomerService.java
+++ src/main/java/com/example/customerrest/service/CustomerService.java
@@ -2,8 +2,15 @@ package com.example.customerrest.service;
 
 import com.example.customerrest.dao.CustomerDao;
 import com.example.customerrest.ds.Customer;
+import com.example.customerrest.ds.CustomerDto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class CustomerService {
@@ -11,11 +18,42 @@ public class CustomerService {
     @Autowired
     private CustomerDao customerDao;
 
-    public void saveCustomer(Customer customer){
-        customerDao.save(customer);
+
+
+    public void saveCustomer(CustomerDto customerDto){
+
+        customerDao.save(toEntity(customerDto));
     }
 
-    public Iterable<Customer> listCustomers(){
-        return customerDao.findAll();
+    public Customer toEntity(CustomerDto customerDto){
+        return new Customer(
+                customerDto.code(),
+                customerDto.firstName(),
+                customerDto.lastName(),
+                customerDto.email()
+        );
+    }
+    public CustomerDto toDto(Customer customer){
+        return new CustomerDto(
+                customer.getId(),
+                customer.getCode(),
+                customer.getFirstName(),
+                customer.getLastName(),
+                customer.getEmail()
+        );
+    }
+    public CustomerDto findCustomerById(int id){
+        Customer customer = customerDao.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+
+        return toDto(customer);
+    }
+
+    public List<CustomerDto> listCustomers(){
+        List<CustomerDto> customers = new ArrayList<>();
+        for (Customer customer:customerDao.findAll()){
+            customers.add(toDto(customer));
+        }
+        return customers;
     }
 }
